eca90157425e824d6121b43edccb31b276c826be
Airam <airam.dev@gmail.com>
2023-02-27 23:24:18 +0000

    Adds win condition

--- src/tic-tac-toe/Board.tsx
+++ src/tic-tac-toe/Board.tsx
@@ -1,25 +1,48 @@
 import Cell from './Cell';
-import {useState} from "react";
+import {useEffect, useState} from 'react';
 
 const Board = () => {
 
   const [gameState, setGameState] = useState({
-    pos0: -1, pos1: -1, pos2: -1,
-    pos3: -1, pos4: -1, pos5: -1,
-    pos6: -1, pos7: -1, pos8: -1
+    0: -1, 1: -1, 2: -1,
+    3: -1, 4: -1, 5: -1,
+    6: -1, 7: -1, 8: -1
   });
   const [currentPlayer, setCurrentPlayer] = useState(0);
+  useEffect(() => {
+    checkWinCondition();
+  },[gameState]);
 
   const handleCellClick = (pos: string) => {
-    setGameState({
-      ...gameState,
-      [pos]: currentPlayer
-    });
-    setCurrentPlayer(prevState => prevState === 0 ? 1 : 0);
+    if (gameState[pos] === -1) {
+      setGameState({
+        ...gameState,
+        [pos]: currentPlayer
+      });
+      setCurrentPlayer(prevState => prevState === 0 ? 1 : 0);
+    }
+  }
+
+  const checkWinCondition = () => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ];
+    winConditions.forEach(condition => {
+      if (gameState[condition[0]] >= 0 && gameState[condition[0]] === gameState[condition[1]] && gameState[condition[0]] === gameState[condition[2]]) {
+        console.log('Winner winner chicken dinner')
+      }
+    })
   }
 
   return (
-    <section className="grid grid-cols-3 gap-1 w-[500px] box-border">
+    <section className="grid grid-cols-3 gap-1 w-[500px] box-border p-2">
       {
         Object.entries(gameState).map(([key, value]) => (
           <Cell key={key} pos={key} value={value} markCell={handleCellClick} />
