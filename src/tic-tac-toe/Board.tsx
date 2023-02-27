745bb4cba244e14417e505754990b65056e01332
Airam <airam.dev@gmail.com>
2023-02-27 21:49:22 +0000

    Adds cell logic

--- src/tic-tac-toe/Board.tsx
+++ src/tic-tac-toe/Board.tsx
@@ -1,13 +1,29 @@
 import Cell from './Cell';
+import {useState} from "react";
 
 const Board = () => {
 
-  const cellsNum: number = 9;
+  const [gameState, setGameState] = useState({
+    pos0: -1, pos1: -1, pos2: -1,
+    pos3: -1, pos4: -1, pos5: -1,
+    pos6: -1, pos7: -1, pos8: -1
+  });
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+
+  const handleCellClick = (pos: string) => {
+    setGameState({
+      ...gameState,
+      [pos]: currentPlayer
+    });
+    setCurrentPlayer(prevState => prevState === 0 ? 1 : 0);
+  }
 
   return (
-    <section className="grid grid-cols-3 gap-1 w-[500px] h-[300px]">
+    <section className="grid grid-cols-3 gap-1 w-[500px] box-border">
       {
-        [...Array(cellsNum)].map((e, i) => <Cell key={i}></Cell>)
+        Object.entries(gameState).map(([key, value]) => (
+          <Cell key={key} pos={key} value={value} markCell={handleCellClick} />
+        ))
       }
     </section>
   );
--- src/tic-tac-toe/Cell.tsx
+++ src/tic-tac-toe/Cell.tsx
@@ -1,8 +1,25 @@
+import './Cell.css';
+import {FC} from 'react';
 
-const Cell = () => {
+interface Props {
+  pos: string;
+  value: number;
+  markCell: Function;
+}
+
+const Cell: FC<Props> = ({pos, value, markCell}) => {
   return (
-    <div className="bg-slate-700 rounded hover:bg-slate-500">
-
+    <div
+      className={`cell h-[150px] bg-slate-700 rounded hover:bg-slate-500 flex justify-center items-center text-6xl ${value === 0 ? "text-rose-500" : "text-sky-500"}`}
+      onClick={() => markCell(pos)}
+    >
+      {
+        value === -1
+          ? ''
+          : value === 0
+            ? 'O'
+            : 'X'
+      }
     </div>
   );
 };
